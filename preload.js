4cc2622ca9bfcc039527653f00ad99c27a44321b
Андрей Трофимов <trofimander48@gmail.com>
2025-05-05 11:23:45 +0300

    fix: Внёс правки

--- preload.js
+++ preload.js
@@ -31,7 +31,7 @@ contextBridge.exposeInMainWorld('preload', {
   },
   playScript: () => {
     console.log('start')
-    interevalId = setInterval(checkForNewFiles, 1000)
+    interevalId = setInterval(checkForNewFiles, 2000)
   },
   stopScript: () => {
     clearInterval(interevalId)
@@ -73,119 +73,23 @@ function checkForNewFiles() {
         return console.error('Ошибка при чтении директории:', err);
       } else if (files.length) {
         files.forEach((file)=>{
-            filesRootPage.forEach((fileName)=>{
-              if (file !== fileName && file !== 'cache' && fileName !== 'cache'){
-                console.log(fileName)
-                filesRootPage.add(file)
-                fs.copyFile(settingsApp[key].folderPath + file, settingsApp[key].folderCache + file.replaceAll(' ', '_'), err => {
-                  if (err) {
-                    console.log('Файл ещё не загружен', err);
-                  } else {
-                    console.log('go');
-                    preloadCopyFile(file, settingsApp[key], filesRootPage);
-                  }
-                })
+          if (file !== 'cache'){
+            filesRootPage.forEach((fileName) => {
+              if (fileName !== 'cache' && fileName !== file) {
+                  fs.copyFile(settingsApp[key].folderPath + file, settingsApp[key].folderCache + file.replaceAll(' ', '_'), err => {
+                    if (err) {
+                      console.log('Файл ещё не загружен...');
+                    } else {
+                      filesRootPage.add(file);
+                      console.log('go');
+                      preloadCopyFile(file, settingsApp[key], filesRootPage);
+                    }
+                  })
               }
             })
+          }
         })
       }
     })
   })
 }
-
-// function parserFile(file, settings) {
-//   const fileNameArr = file.replace(/[.()-,]/g, '_').toUpperCase().split('_');
-//       additionalSettings(fileNameArr, settings)
-//       let arrTag = fileNameArr.filter((item) => { return settings.listTag.indexOf(item) >= 0; });
-//       if (arrTag.length) {
-//         let tag = arrTag.join('');
-//         if (arrTag.length > 1) {
-//           if (settings.dirList[arrTag.join('_')]) {
-//             tag = arrTag.join('_');
-//           } else {
-//             tag = arrTag.reverse().join('_');
-//           }
-//         }
-//         if (settings.dirList[tag]) {
-//           fs.copyFile(settings.folderPath + file, settings.dirList[tag] + file.replaceAll(' ', '_'), err =>{
-//             if (err){
-//               console.log('Ошибка копирования ' + err);
-//             }else{
-//               console.log(`Файл ${file} перемещён в ${settings.dirList[tag]}`);
-//               fs.unlink(settings.folderPath + file, (err) => {
-//                 if (err) {
-//                   console.log('Ошибка удаления ' + err)
-//                 } else {
-//                   if (filesRootPage.includes(file)) {
-//                     let newArr = filesRootPage.filter((item) => {
-//                       return item !== file
-//                     })
-//                     filesRootPage = newArr;
-//                     return filesRootPage
-//                   }
-//                 }
-//                 return filesRootPage
-//               });
-//               return filesRootPage
-//             }
-            
-//           });
-//           return filesRootPage
-//         } else {
-//           fs.copyFile(settings.folderPath + file, settings.dirDefault + file.replaceAll(' ', '_'), err => {
-//             if (err) {
-//               console.log('Ошибка копирования в дефолтную папку ' + err)
-//             }else{
-//               console.log(`ERROR: НЕ НАЙДЕНА ПАПКА С ИМЕНЕМ ${tag}! Файл ${file} перемещён в дефолтную папку ${settings.dirDefault}. Проверьте имя файла или создайте нужную папку.`);
-//               fs.unlink(settings.folderPath + file, (err) => {
-//                 if (err) {
-//                   console.log('Ошибка удаления ' + err)
-//                 } else {
-//                   if (filesRootPage.includes(file)) {
-//                     let newArr = filesRootPage.filter((item) => {
-//                       return item !== file
-//                     })
-//                     filesRootPage = newArr;
-//                     return filesRootPage
-//                   }
-//                 }
-//                 return filesRootPage
-//               });
-//               return filesRootPage
-//             }
-//           });
-//         }
-//       } else {
-//         fs.copyFile(settings.folderPath + file, settings.dirDefault + file.replaceAll(' ', '_'), err => {
-//           if (err) {
-//             console.log('Ошибка копирования в дефолтную папку ' + err)
-//           }else{
-//             console.log(`ERROR: НЕТ СОВПАДЕНИЙ! Файл ${file} перемещён в дефолтную папку ${settings.dirDefault}.`);
-//             fs.unlink(settings.folderPath + file, (err) => {
-//               if (err) {
-//                 console.log('Ошибка удаления ' + err)
-//               } else {
-//                 if (filesRootPage.includes(file)) {
-//                   let newArr = filesRootPage.filter((item) => {
-//                     return item !== file
-//                   })
-//                   filesRootPage = newArr;
-//                   return filesRootPage
-//                 }
-//               }
-//               return filesRootPage
-//             });
-//             return filesRootPage
-//           }
-//         });
-//       }
-// }
-
-// function additionalSettings(arr, settings) {
-//   if (settings.defaultTagSwitch) {
-//     if (arr.length == 2 && Number(arr[0])) { arr.push(settings.defaultTag) }
-//   }
-//   if (!settings.typeSortFormatFile) {
-//     arr.pop()
-//   }
-// }
