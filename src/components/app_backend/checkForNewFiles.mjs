1b7fa3685a734e99a6d90e7658e25be8b2faf4cc
Андрей Трофимов <trofimander48@gmail.com>
2025-06-23 16:18:40 +0300

    feat: добавил заготовку для изменения алгоритма проверки наличия новых файлов в папках

--- src/components/app_backend/checkForNewFiles.mjs
+++ src/components/app_backend/checkForNewFiles.mjs
@@ -1,5 +1,6 @@
 const { copyFile, GetCopyFileParam } = require('./components/app_backend/copyFile.mjs');
 const fs = require('fs');
+const chokidar = require('chokidar');
 const filesNameCache = new Set();
 
 export function checkForNewFiles(param) {
@@ -23,4 +24,37 @@ export function checkForNewFiles(param) {
             }
         })
     })
+}
+
+export class CheckForNewFiles{
+    settingsFileDir
+    settingsApp
+    rootFoldersPath
+    watcher
+    constructor(settingsFile){
+        this.settingsFileDir = settingsFile
+        this.settingsApp = JSON.parse(fs.readFileSync(settingsFile, 'utf8'))
+    }
+    #setRootFoldersPath(){
+        this.rootFoldersPath = []
+        const settingKeys = Object.keys(this.settingsApp)
+        if (settingKeys.length){
+            settingKeys.forEach((key) => {
+                this.rootFoldersPath.push(this.settingsApp[key].folderPath)
+            })
+        }else {
+             console.log('Добавьте папку для отслеживания')
+        }
+    }
+    watchPlay(){
+        this.#setRootFoldersPath()
+        this.watcher = chokidar.watch(this.rootFoldersPath, { awaitWriteFinish: { stabilityThreshold: 2000, pollInterval: 100 }})
+        this.watcher
+            .on('change', path => console.log(`Файл ${path} добавлен`))
+            .on('unlink', path => console.log(`Файл ${path} удалён`))
+    }
+    watchStop(){
+        this.watcher.unwatch(this.rootFoldersPath)
+        this.watcher.close()
+    }
 }
--- src/preload.js
+++ src/preload.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const { contextBridge, ipcRenderer } = require('electron');
 const { frontConfig, messageColor } = require('./components/app_frontend/js/config/config.mjs')
-const { checkForNewFiles } = require('./components/app_backend/checkForNewFiles.mjs');
+const { checkForNewFiles, CheckForNewFiles } = require('./components/app_backend/checkForNewFiles.mjs');
 const { SettingsApi } = require('./components/app_backend/settingsApi.mjs');
 const { NewRootFolder } = require('./components/app_backend/newRootFolder.mjs');
 const { AddFolder } = require('./components/app_backend/addFolder.mjs');
@@ -10,6 +10,7 @@ const { addNewTag } = require('./components/app_backend/addNewTag.mjs');
 let interevalId;
 
 const settingsFile = './resources/settings.json';
+const checkerFiles = new CheckForNewFiles(settingsFile);
 contextBridge.exposeInMainWorld('preload', {
   frontConfig: ()=> {
     return frontConfig
@@ -30,12 +31,14 @@ contextBridge.exposeInMainWorld('preload', {
     addNewTag({ submitData, rootFolderId, renderTagItem, SettingsApi, settingsFile })
   },
   playScript: (addLogMessage) => {
-    addLogMessage('start', messageColor.ok)
-    interevalId = setInterval(() => { checkForNewFiles({ settingsFile, addLogMessage, messageColor }) }, 2000)
+    checkerFiles.watchPlay()
+    // addLogMessage('start', messageColor.ok)
+    // interevalId = setInterval(() => { checkForNewFiles({ settingsFile, addLogMessage, messageColor }) }, 2000)
   },
   stopScript: (addLogMessage) => {
-    clearInterval(interevalId);
-    addLogMessage('stop', messageColor.error)
+    checkerFiles.watchStop()
+    // clearInterval(interevalId);
+    // addLogMessage('stop', messageColor.error)
   },
   getSettings: (settingsFile, settingsTemplate) => {
     const settingsApi = new SettingsApi(settingsFile, settingsTemplate);
