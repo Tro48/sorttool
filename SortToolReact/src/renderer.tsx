63f2f29162858a6f98534e54a2bd5fbbc8e7c2ac
Andrey Trofimov <trofimander48@gmail.com>
2025-07-03 23:47:59 +0300

    fix: поправил отображение иконки в трее. Настроил линтер

--- SortToolReact/src/renderer.tsx
+++ SortToolReact/src/renderer.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { App } from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { App } from './App'
 declare global {
-  interface Window {
-    [key: string]: any;
-  }
+	interface Window {
+		[key: string]: any
+	}
 }
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+)
